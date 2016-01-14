83e90c01f7ff39dfa49b20e393d9679d6557e72e
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-01-14 14:03:03 -0800

    refactor mac version to fetch with mix

--- install.exs
+++ install.exs
@@ -40,37 +40,6 @@ defmodule BakeInstaller do
 
   def ps_mkdirp(path), do: "New-Item -path \"#{path}\" -Force -type directory"
 
-  def windows_install_bake(install_prefix, bake_home) do
-    win = [powershell: true]
-    puts "=> Creating bake home"
-    bake_home |> ps_mkdirp() |> system(win)
-
-    tarball = "#{bake_home}\\bake.tar.gz"
-    untar = "#{bake_home}\\go-untar.exe"
-
-    puts "=> Downloading latest bake"
-    :ok = get(@bake_url, tarball, @bake_sha512)
-
-    puts "=> Downloading untar tool"
-    :ok = get(@win32_untar_url, untar, @win32_untar_sha512)
-
-    {_, exitstatus} = install_prefix |> ps_mkdirp() |> system(win)
-    if exitstatus != 0 do
-      raise "Error creating installation directory #{install_prefix}"
-    end
-
-    extract = "Set-Location #{install_prefix}; #{untar} #{tarball}"
-    {_, exitstatus} = extract |> system(win)
-    if exitstatus != 0 do
-      raise "Bake did not install correctly. Check permissions on #{install_prefix}."
-    end
-
-    system("Remove-Item #{tarball}", win)
-    system("Remove-Item #{untar}", win)
-
-    puts "=> bake version #{@bake_version} installed to #{install_prefix}"
-  end
-
   def osx_install_deps do
     {_, exitstatus} = system("brew --version")
     if exitstatus != 0 do
@@ -107,19 +76,17 @@ defmodule BakeInstaller do
   def unix_install_bake(install_prefix, bake_home) do
     puts "=> Creating bake home"
     system("mkdir -p #{bake_home}")
+    tarball = "#{bake_home}/bake.tar.gz"
 
     puts "=> Downloading latest bake"
-    {_, exitstatus} = system("curl -o #{bake_home}/bake.tar.gz -L https://s3.amazonaws.com/bakeware/bake/bake-#{@bake_version}.tar.gz")
-    if exitstatus != 0 do
-      raise "Failed to download bake tarball!"
-    end
+    :ok = get(@bake_url, tarball, @bake_sha512)
 
     {_, exitstatus} = system("mkdir -p #{install_prefix}")
     if exitstatus != 0 do
       raise "Error creating installation directory #{install_prefix}"
     end
 
-    {_, exitstatus} = system("tar -xf #{bake_home}/bake.tar.gz -C #{install_prefix}")
+    {_, exitstatus} = system("tar -xf #{tarball} -C #{install_prefix}")
     if exitstatus != 0 do
       raise "Bake did not install correctly. Check permissions on #{install_prefix}."
     end
@@ -129,8 +96,38 @@ defmodule BakeInstaller do
     puts "=> bake version #{@bake_version} installed to #{install_prefix}"
   end
 
+  def windows_install_bake(install_prefix, bake_home) do
+    win = [powershell: true]
+    puts "=> Creating bake home"
+    bake_home |> ps_mkdirp() |> system(win)
+
+    tarball = "#{bake_home}\\bake.tar.gz"
+    untar = "#{bake_home}\\go-untar.exe"
+
+    puts "=> Downloading latest bake"
+    :ok = get(@bake_url, tarball, @bake_sha512)
+
+    puts "=> Downloading untar tool"
+    :ok = get(@win32_untar_url, untar, @win32_untar_sha512)
+
+    {_, exitstatus} = install_prefix |> ps_mkdirp() |> system(win)
+    if exitstatus != 0 do
+      raise "Error creating installation directory #{install_prefix}"
+    end
+
+    {_, exitstatus} = "Set-Location #{install_prefix}; #{untar} #{tarball}" |> system(win)
+    if exitstatus != 0 do
+      raise "Bake did not install correctly. Check permissions on #{install_prefix}."
+    end
+
+    system("Remove-Item #{tarball}; Remove-Item #{untar}", win)
+
+    puts "=> bake version #{@bake_version} installed to #{install_prefix}"
+  end
+
+
   def init({:unix, :linux}) do
-    home_dir = "~/.bake"
+    home_dir = "#{System.get_env("HOME")}/.bake"
     install_dir = home_dir <> "/bin"
 
     linux_install_deps
@@ -141,7 +138,7 @@ defmodule BakeInstaller do
 
   def init({:unix, :darwin}) do
     osx_install_deps
-    unix_install_bake('/usr/local/bin', '~/.bake')
+    unix_install_bake('/usr/local/bin', "#{System.get_env("HOME")}/.bake")
   end
 
   def init({:win32, :nt}) do
